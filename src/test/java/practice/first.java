b50e5e4927d04cfc65adb48036bcb9f3b2dd907c
kranthi <kranthidevavarapu@gmail.com>
2018-10-27 15:49:00 -0400

    first

---
+++ src/test/java/practice/first.java
@@ -0,0 +1,17 @@
+package practice;
+
+public class first {
+        public static void main(String[] args) {
+
+            int i;
+            int[] arr = {1, 2, 3};
+            for (i = 0; i <= 2; i++) {
+                System.out.println("print" + i);
+
+
+            }
+        }
+    }
+
+
+
